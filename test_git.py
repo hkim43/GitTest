47681e534c57fb57dc3d6c74aac47beb8f4033e3
John Kim <John@Johns-MBP-4.localdomain>
2019-06-07 14:50:37 -0400

    final product

--- test_git.py
+++ test_git.py
@@ -1,36 +1,14 @@
 SITES = [
-<<<<<<< HEAD
-   "https://test1.com"
-   ""https://test1.com""
+   "https://test1.com",
+   "https://test2.com",
    "https://test3.com",
    "https://test4.com",
    "https://test5.com",
    "https://test6.com",
    "https://test7.com",
-   SITES = [
-   "https://test1.com"
-   ""https://test1.com""
-   "https://test3.com",
-   "https://test4.com",
-   "https://test5.com",
-   "https://test6.com",
-   "https://test7.com",
-   "https://test8.com",
-   "https://test9.com",
-   "https://test10.com"
-]
-
-   "https://test8.com",
-   "https://test9.com",
-   "https://test10.com"
-]
-=======
-	"https://test1.com": "John Kim",
-	"https://test2.com": "John Kim",
-	"https://test3.com": "John Kim",
-	"https://test4.com": "John Kim",
-	"https://test5.com": "John Kim",
-	"https://test6.com": "John Kim",
-	"https://test7.com"
-]
->>>>>>> add-5-names
+   "John Kim",
+   "John Kim",
+   "John Kim",
+   "John Kim",
+   "John Kim",
+   ]
