9b5bc2ab6566365b5ffee5b65415a08395ce06b0
John Kim <John@Johns-MBP-4.localdomain>
2019-06-07 13:58:02 -0400

    add-5-names

--- test_git.py
+++ test_git.py
@@ -1,9 +1,9 @@
 SITES = [
 	"https://test1.com": "John Kim",
-	"https://test2.com",
-	"https://test3.com",
-	"https://test4.com",
-	"https://test5.com",
-	"https://test6.com",
+	"https://test2.com": "John Kim",
+	"https://test3.com": "John Kim",
+	"https://test4.com": "John Kim",
+	"https://test5.com": "John Kim",
+	"https://test6.com": "John Kim",
 	"https://test7.com"
 ]
