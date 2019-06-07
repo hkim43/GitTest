212ba847a01c18ebffcca54f51fb02ce4fa77abe
John Kim <John@Johns-MBP-4.localdomain>
2019-06-07 16:02:36 -0400

    first commit

--- test_git.py
+++ test_git.py
@@ -1,14 +1,9 @@
 SITES = [
    "https://test1.com",
-   "https://test2.com",
    "https://test3.com",
    "https://test4.com",
    "https://test5.com",
    "https://test6.com",
    "https://test7.com",
-   "John Kim",
-   "John Kim",
-   "John Kim",
-   "John Kim",
-   "John Kim",
-   ]
+   "John Kim"
+]
